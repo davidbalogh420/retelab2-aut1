fa8b79f14d0562ba2fa2e62f53c4f3b0fbc2858e
davidbalogh420 <dave.balogh02@gmail.com>
2023-09-24 16:11:00 +0200

    1. feladat DONE

---
+++ src/main/java/hu/bme/aut/retelab2/domain/Ad.java
@@ -0,0 +1,72 @@
+package hu.bme.aut.retelab2.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Ad {
+
+
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private int price;
+
+
+    private LocalDateTime date;
+
+    @PrePersist
+    protected void onCreate() {
+        date = LocalDateTime.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+}
